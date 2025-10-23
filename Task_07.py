a1d6847b469cbf38ebd5eb0ebeded16d74b3a91a
Chris Bucknell <99325130+C-Bucknell@users.noreply.github.com>
2025-10-23 12:13:33 +1100

    Update Task_07.py
    Updated by teacher to remove error for assessment

--- Task_07.py
+++ Task_07.py
@@ -27,11 +27,11 @@ tests above produce the given outputs.
 # All code to debug is below here
 #==============================
 def hello(name):
-    # Updated by teacher to remove errors == greeting = (f'Hello, {name}'!')
+    greeting = (f'Hello, {name}!')
     return greeting
 
 def main():
-  name = int(input('What is your name? '))
+  name = input('What is your name? ')
     '''
     Updated by teacher to remove errors
   if len(name < 3:
